205300d1b1888150cdbddae99f4546d5b17e56d0
Emma <furlan.emmanuelle@neuf.fr>
2025-04-23 20:31:24 +0200

    Tokenize the input into an array of arguments

--- token.c
+++ token.c
@@ -1,49 +1,81 @@
 #include "main.h"
 
+#define MAX_ARGS 64
+
 /**
- * tokenize - Splits the input string into tokens (arguments).
- * @input: User input string.
- * @args: Array to store the tokens.
+ * free_args - Free each element of the argument array
+ * @array_command: An array of command strings
  */
-void tokenize(char *input, char *args[])
+void free_args(char **array_command)
+{
+int i;
+
+if (array_command == NULL)
+return;
+
+for (i = 0; array_command[i] != NULL; i++)
+free(array_command[i]);
+
+free(array_command);
+}
+
+/**
+ * get_argument - Tokenize the input into an array of arguments
+ * @input: Input string to tokenize
+ * Return: Array of arguments
+ */
+char **get_argument(char *input)
 {
 char *token;
-unsigned int i = 0;
+char **args = malloc(sizeof(char *) * MAX_ARGS);
+int i = 0;
 
-token = strtok(input, " ");
-
-while (token != NULL)
+if (args == NULL)
 {
-args[i] = token;
+perror("malloc");
+exit(EXIT_FAILURE);
+}
+
+token = strtok(input, " \t\r\n\a");
+while (token && i < MAX_ARGS - 1)
+{
+args[i] = strdup(token);
+if (args[i] == NULL)
+{
+perror("strdup");
+free_args(args);
+exit(EXIT_FAILURE);
+}
+token = strtok(NULL, " \t\r\n\a");
 i++;
-token = strtok(NULL, " ");
 }
 args[i] = NULL;
 
+return (args);
+}
+
+/**
+ * handle_builtin - Check and handle built-in commands
+ * @args: The argument array
+ * Return: 1 if built-in was handled, 0 otherwise
+ */
+int handle_builtin(char **args)
+{
 if (args[0] == NULL)
-_exit(0);
+return (1);
+
+if (strcmp(args[0], "exit") == 0)
+{
+free_args(args);
+exit(0);
+}
 
 if (strcmp(args[0], "env") == 0)
 {
-printEnv();
-return;
+print_env(); /* Ta fonction d'affichage de l'env */
+free_args(args);
+return (1);
 }
 
-if (strcmp(args[0], "exit") == 0 && args[1] == NULL)
-{
-free(args[0]);
-_exit(0);
-}
-
-char *path = handle_path(args[0]);
-if (path != NULL)
-{
-execve(path, args, environ);
-free(path);
-}
-else
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-_exit(127);
-}
+return (0);
 }
