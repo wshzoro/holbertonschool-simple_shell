3edfaafc2f233881b0051aaeeba74942298bdf25
Najwarci <rachdi.najwa91@gmail.com>
2025-04-23 19:03:39 +0200

    Get the value of an environment variable

--- env.c
+++ env.c
@@ -1,14 +1,26 @@
 #include "main.h"
-/**
- * print_env - print environment
- */
-void print_env(void)
-{
-char **env = environ;
 
-while (*env)
+/**
+ * genv - Get the value of an environment variable
+ * @name: Name of the environment variable to retrieve
+ *
+ * Return: Pointer to the value string of the environment variable,
+ *         or NULL if the variable is not found.
+ */
+char *genv(const char *name)
 {
-printf("%s/n", *env);
-env++;
+char **env;
+size_t len;
+
+if (name == NULL || environ == NULL)
+return (NULL);
+
+len = strlen(name);
+
+for (env = environ; *env != NULL; env++)
+{
+if (strncmp(*env, name, len) == 0 && (*env)[len] == '=')
+return (*env + len + 1);
 }
+return (NULL);
 }
